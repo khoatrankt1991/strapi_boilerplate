30571f4ff9722f0f4e75751d492ce28ef2f426aa
0xKoddoelo <2apple_q2@2Apple-Q2s-MacBook-Pro.local>
2018-12-28 12:54:22 +0700

    Add Custom API

---
+++ api/all/controllers/All.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * Block.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Block`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve block records.
+   *
+   * @return {Object|Array}
+   */
+
+  aboutall: async (ctx) => {
+    return {
+      about: await strapi.services.about.fetchAll(ctx.query),
+      jobs: await strapi.services.job.fetchAll(ctx.query),
+      skills: await strapi.services.skill.fetchAll(ctx.query),
+      blocks: await strapi.services.block.fetchAll(ctx.query)
+    };
+  },
+};
